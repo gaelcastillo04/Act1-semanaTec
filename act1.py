ffef29d00307988f44dc78998f2721ba887d09fd
gaelcastillo04 <a01638638@tec.mx>
2025-05-06 08:02:09 -0600

    If applied, this commit will fix spacing in operators and remove unused library

--- act1.py
+++ act1.py
@@ -1,26 +1,23 @@
 import cv2
 import easyocr
-import numpy as np
+
 
 
 def procesar_imagen(nombre_archivo):
-    #leemmos la imagen 
-    imagen=cv2.imread(nombre_archivo)
-    #convertimos a gray scale
-    imagen_gray=cv2.cvtColor(imagen, cv2.COLOR_BGR2GRAY)
-    cv2.imwrite(f"{nombre_archivo}_original.png",imagen_gray)
-    #treshold nos ayuda a trabajar con grises y sirve para transformar la intensidad
-    ret, thresh=cv2.threshold(imagen_gray,200,255,cv2.THRESH_BINARY_INV)
-    cv2.imwrite(f"{nombre_archivo}_thresh.png",thresh)
-    #suavizamos la imagen (Gaussian filter)
-    img_gaussian=cv2.GaussianBlur(src=thresh,ksize=(37,37),sigmaX=0)
+    imagen=cv2.imread(nombre_archivo)  #leemmos la imagen 
+    imagen_gray= cv2.cvtColor(imagen, cv2.COLOR_BGR2GRAY)  #convertimos a gray scale
+    cv2.imwrite(f"{nombre_archivo}_original.png", imagen_gray)
+    ret, thresh= cv2.threshold(imagen_gray, 200, 255, cv2.THRESH_BINARY_INV)  #treshold nos ayuda a trabajar con grises y sirve para transformar la intensidad
+    cv2.imwrite(f"{nombre_archivo}_thresh.png", thresh)
+    img_gaussian= cv2.GaussianBlur(src=thresh, ksize=(37, 37), sigmaX=0)  #suavizamos la imagen (Gaussian filter)
     cv2.imwrite(f"{nombre_archivo}_gaussian.png", img_gaussian)
     #usamos easy orc
-    reader=easyocr.Reader(["es","en"],verbose=False)
+    reader= easyocr.Reader(["es", "en"], verbose=False)  
     result=reader.readtext(img_gaussian)
-    text_detected="\n".join([text for _,text, _ in result])
+    text_detected="\n".join([text for _, text, _ in result])
     print("texto detectado: ")
     print(text_detected)
+
 
 #Llamamos la funcion con ambas imagenes
 procesar_imagen("placa_q.jpg")
