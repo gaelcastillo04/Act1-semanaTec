4a57548a9101d1c307ea9b549a4a888801bfb24d
gaelcastillo04 <a01638638@tec.mx>
2025-05-06 08:29:31 -0600

    If applied, this commit will add indentation, inline comments and respect de 72 max caracter per line.

--- act1.py
+++ act1.py
@@ -1,16 +1,18 @@
 import cv2
 import easyocr
 
-#Esta funcion procesa una imagen y imprime el texto detectado.
+# Esta funcion procesa una imagen y imprime el texto detectado.
 def procesar_imagen(nombre_archivo):
-    imagen=cv2.imread(nombre_archivo)  #leemmos la imagen 
-    imagen_gray= cv2.cvtColor(imagen, cv2.COLOR_BGR2GRAY)  #convertimos a gray scale
+    imagen=cv2.imread(nombre_archivo)  # Leemmos la imagen 
+    imagen_gray= cv2.cvtColor(imagen, cv2.COLOR_BGR2GRAY)  # Convertimos a gray scale
     cv2.imwrite(f"{nombre_archivo}_original.png", imagen_gray)
-    ret, thresh= cv2.threshold(imagen_gray, 200, 255, cv2.THRESH_BINARY_INV)  #treshold nos ayuda a trabajar con grises y sirve para transformar la intensidad
+    # Treshold nos ayuda a trabajar con grises y sirve para transformar la intensidad
+    ret, thresh= cv2.threshold(imagen_gray, 200, 255, cv2.THRESH_BINARY_INV)  
     cv2.imwrite(f"{nombre_archivo}_thresh.png", thresh)
-    img_gaussian= cv2.GaussianBlur(src=thresh, ksize=(37, 37), sigmaX=0)  #suavizamos la imagen (Gaussian filter)
+    # Suavizamos la imagen (Gaussian filter)
+    img_gaussian= cv2.GaussianBlur(src=thresh, ksize=(37, 37), sigmaX=0)  
     cv2.imwrite(f"{nombre_archivo}_gaussian.png", img_gaussian)
-    #usamos easy orc
+    # Usamos easy orc
     reader= easyocr.Reader(["es", "en"], verbose=False)  
     result=reader.readtext(img_gaussian)
     text_detected="\n".join([text for _, text, _ in result])
